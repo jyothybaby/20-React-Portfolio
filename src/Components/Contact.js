22025fed21977da5dca903cfc6f5bb31731160cd
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-11-14 01:10:54 -0500

    Added validation to the Contact Form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 
 class Contact extends Component {
+   
   render() {
 
-    if(this.props.data){
+   if(this.props.data){
       var name = this.props.data.name;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
@@ -12,6 +13,66 @@ class Contact extends Component {
       var phone= this.props.data.phone;
       var email = this.props.data.email;
       var message = this.props.data.contactmessage;
+    }
+
+   var contactName = '';
+   var contactEmail = '';
+   var contactMessage = '';
+
+
+   const handleInputChange = (e) => {
+      // Getting the value and name of the input which triggered the change
+      const { target } = e;
+      const inputType = target.name;
+      const inputValue = target.value;
+  
+      // Based on the input type, we set the state of either email, username, and password
+      if (inputType === 'contactEmail') {
+         contactEmail = inputValue;
+      } else if (inputType === 'contactName') {
+         contactName = inputValue;
+      } else if (inputType === 'contactMessage') {
+         contactMessage = inputValue;
+      }
+    };
+
+    const handleFormSubmit = (e) => {
+      // Preventing the default behavior of the form submit (which is to refresh the page)
+      e.preventDefault();
+
+      if(contactName == ''){
+         alert('Name is mandatory');
+         return;
+      }
+
+      if(contactEmail == ''){
+         alert('Email is mandatory');
+         return;
+      }
+
+      // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+      if (!validateEmail(contactEmail)) {
+         alert( 'Email is invalid');
+         
+         // We want to exit out of this code block if something is wrong so that the user can correct it
+         return;
+         // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+       }
+
+      if(contactMessage == ''){
+         alert('Message is mandatory');
+         return;
+      }
+  
+      alert('Contact request sent successfully');
+      
+      // If everything goes according to plan, we want to clear out the input after a successful registration.
+      //contactEmail = '';
+    }; 
+ 
+    function validateEmail(email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
     }
 
     return (
@@ -41,32 +102,34 @@ class Contact extends Component {
 
                   <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
+						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={handleInputChange} />
                   </div>
 
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
+						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={handleInputChange} />
                   </div>
 
-                  <div>
+                  {/* <div>
 						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
-                  </div>
+						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={handleInputChange} />
+                  </div> */}
 
                   <div>
                      <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage" onChange={handleInputChange}></textarea>
                   </div>
 
                   <div>
-                     <button className="submit">Submit</button>
+                     <button className="submit" onClick={handleFormSubmit}>Submit</button>
                      <span id="image-loader">
                         <img alt="" src="images/loader.gif" />
                      </span>
                   </div>
 					</fieldset>
 				   </form>
+
+              
 
            <div id="message-warning"> Error boy</div>
 				   <div id="message-success">
