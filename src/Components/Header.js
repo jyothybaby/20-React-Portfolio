697730c0bf1f9da8a8645dbab162f5faaa636ca5
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-11-14 22:34:16 -0500

    updating homepage in navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -21,7 +21,7 @@ class Header extends Component {
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
          <div>
-         <a href="/">Jyothy V Baby</a>
+         <a href="/20-React-Portfolio">Jyothy V Baby</a>
          <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
